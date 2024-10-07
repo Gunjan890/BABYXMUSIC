ae60b82e3068c1e194410dfacbda64a4940fd407
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-10-08 01:03:45 +0530

    Update couples.py

--- SONALI/plugins/tools/couples.py
+++ SONALI/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from SONALI.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="|| ˹𖤍 𝗔𝗟𝗣𝗛𝗔 𖤍˼ ||",
-            url=f"https://t.me/PURVI_SUPPORT",
+            text="|| ˹𖤍 🇬𝐮𝐧𝐣𝐚𝐧🐬 𖤍˼ ||",
+            url=f"https://t.me/+TkkVwWjSqWc5MDc1",
         ),
     ],
 ]
