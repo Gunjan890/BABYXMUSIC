654cd6f5d18ac63bbd9a363fe9b53ebb108dddd8
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-10-12 23:42:28 +0530

    Update gitinfo.py

--- SONALI/plugins/tools/gitinfo.py
+++ SONALI/plugins/tools/gitinfo.py
@@ -10,7 +10,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 @app.on_message(filters.command(["github", "git"]))
 async def github(_, message):
     if len(message.command) != 2:
-        await message.reply_text("/git TEAMPURVI")
+        await message.reply_text("/git 🇬𝐮𝐧𝐣𝐚𝐧🐬")
         return
 
     username = message.text.split(None, 1)[1]
