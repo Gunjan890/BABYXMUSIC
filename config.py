c7fb73b13c5689bc91194ca36879d035c3b3b986
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2025-06-13 07:43:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -51,8 +51,8 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN","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")  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/irotechlab")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ironmanhindigming1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PURVI_SUPPORT")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PURVI_UPDATES")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
