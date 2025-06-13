b4f67ee0ed8197acb5e71165f5e2ead4aecdd29a
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2025-06-13 07:38:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,11 +13,11 @@ API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 # -------------------------------------------------------
-OWNER_USERNAME = getenv("OWNER_USERNAME","II_apna_gunjan_II")
+OWNER_USERNAME = getenv("OWNER_USERNAME","Ironmanhindigaming")
 # --------------------------------------------------------
-BOT_USERNAME = getenv("BOT_USERNAME","baby_xusic_bot")
+BOT_USERNAME = getenv("BOT_USERNAME","irotechmusicbot")
 # --------------------------------------------------------
-BOT_NAME = getenv("BOT_NAME")
+BOT_NAME = getenv("BOT_NAME","IRO MUSIC")
 # ---------------------------------------------------------
 
 
@@ -27,32 +27,32 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", None))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002311769574))
 
 # Get this value from @PURVI_HELP_BOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 7704626820))
+OWNER_ID = int(getenv("OWNER_ID", 5909658683))
+
 
 # make your bots privacy from telegra.ph and put your url here 
 PRIVACY_LINK = getenv("PRIVACY_LINK", "https://graph.org/PRIVACY-FOR-TEAM-PURVI-BOTS-09-18")
-
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+API_KEY = getenv("API_KEY", '30DxNexGenBotsbe462b') # youtube song api key
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Gunjan890/BABYXMUSIC",
+    "https://github.com/IRONBOY112/IROTECH-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
-)  # Fill this variable if your upstream repository is private
+    "GIT_TOKEN","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")  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+YOIkTivyXMgxNzFl")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+YOIkTivyXMgxNzFl")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/irotechlab")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ironmanhindigming1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -90,21 +90,20 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/n50arv.jpg"
-)
+    "START_IMG_URL", "https://iili.io/3tYLHes.jpg")
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/iiw5rn.jpg"
+    "PING_IMG_URL", "https://iili.io/3tYLHes.jpg"
 )
-PLAYLIST_IMG_URL = "https://telegra.ph/file/8ff7a386f161aea9ed5fb.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/51cb8a22e65caa4382879.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/51cb8a22e65caa4382879.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/51cb8a22e65caa4382879.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://files.catbox.moe/dzjerj.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+PLAYLIST_IMG_URL = "https://iili.io/3tYLHes.jpg"
+STATS_IMG_URL = "https://iili.io/3tYLHes.jpg"
+TELEGRAM_AUDIO_URL = "https://iili.io/3tYLHes.jpg"
+TELEGRAM_VIDEO_URL = "https://iili.io/3tYLHes.jpg"
+STREAM_IMG_URL = "https://iili.io/3tYLHes.jpg"
+SOUNCLOUD_IMG_URL = "https://iili.io/3tYLHes.jpg"
+YOUTUBE_IMG_URL = "https://iili.io/3tYLHes.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://iili.io/3tYLHes.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://iili.io/3tYLHes.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://iili.io/3tYLHes.jpg"
 
 
 def time_to_seconds(time):
