4d62ecf57d4bd2c20b1087237afd0c108c04b3dc
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2025-06-28 14:42:14 +0530

    Update phone.py

--- SONALI/plugins/tools/phone.py
+++ SONALI/plugins/tools/phone.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 import requests
 import json
-from SACHIN_MUSIC import app
+from SONALI import app
 
 def send_message(message, text):
     message.reply_text(text)
